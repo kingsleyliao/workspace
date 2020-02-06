e577e2732bf3c021d39f5bed52af6bc5ed225ced
Kingsley Liao <kingsleyyliao@gmail.com>
2020-02-06 03:49:53 -0500

    Add user object

--- app/src/main/java/com/example/workspace/network/ApiService.java
+++ app/src/main/java/com/example/workspace/network/ApiService.java
@@ -1,6 +1,7 @@
 package com.example.workspace.network;
 
 import com.example.workspace.models.Post;
+import com.example.workspace.models.User;
 
 import java.util.List;
 
@@ -10,4 +11,7 @@ import retrofit2.http.GET;
 public interface ApiService {
     @GET("posts")
     Observable<List<Post>> fetchPosts();
+
+    @GET("users")
+    Observable<List<User>>fetchUsers();
 }
